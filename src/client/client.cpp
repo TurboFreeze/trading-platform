3b37a9baf66a73c015ec530ea47889d3ff99df7b
TurboFreeze <iturbofreeze@gmail.com>
2018-06-05 20:49:49 -0400

    Finished client-side socket connection work

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1,12 +1,17 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <iostream>
+#include <unistd.h>
 
 int main (void) {
+        int socketfd;
         char* destination = "localhost";
         char* port = "102";
         struct addrinfo hints;
         struct addrinfo *res;
+        struct addrinfo *result;
+
+        std::cout << "Attempting to reach server..." << std::endl;
 
         // struct setup
         memset(&hints, 0, sizeof hints);
@@ -16,6 +21,32 @@ int main (void) {
                 perror("could not get info");
                 return 1;
         }
+
+        // loop through for first possible result
+
+        for (result = res; result != NULL; result = result->ai_next) {
+                if ((socketfd = socket(result->ai_family, result->ai_socktype, result->ai_protocol)) == -1) {
+                        perror("socket failure");
+                        return 1;
+                }
+
+                // attempt to make the connection
+                if (connect(socketfd, result->ai_addr, result->ai_addrlen) == -1) {
+                        close(socketfd);
+                        perror("connection");
+                        continue;
+                }
+                break;
+        }
+
+        // finish connecting by freeing up and checking for success
+        freeaddrinfo(res);
+        if (result == NULL) {
+                perror("conneciton failed");
+                return 1;
+        }
+
+        std::cout << "Connected to server" << std::endl;
 
         return 0;
 }
